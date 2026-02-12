e431c06abe8c619f297c198abfd8aa4dced97670
André Montenegro <126609983+andre4383@users.noreply.github.com>
2026-02-12 16:14:07 -0300

    Arquivos

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center px-8">
+      <div>
+        <h1 className="text-5xl font-bold">are you ok?</h1>
+        <p className="text-muted-foreground text-gray-500">welcome back</p>
+      </div>
+    </div>
+  );
+}
