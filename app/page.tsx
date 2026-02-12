fa22638250bb4855adaeda58af81cab12df2aba4
André Montenegro <126609983+andre4383@users.noreply.github.com>
2026-02-12 16:56:36 -0300

    Arquivos

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
-import Image from "next/image";
+"use client";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
 export default function Home() {
+  const text = "are you ok?";
+
+  const textRef = useRef<HTMLHeadingElement>(null);
+
+  useGSAP(() => {
+    if (!textRef.current) return;
+
+    const letters = textRef.current.querySelectorAll("span");
+
+    gsap.from(letters, {
+      opacity: 0,
+      stagger: 0.4,
+    });
+  });
+
   return (
     <div className="min-h-screen flex items-center px-8">
       <div>
-        <h1 className="text-5xl font-bold">are you ok?</h1>
-        <p className="text-muted-foreground text-gray-500">welcome back</p>
+        <h1 ref={textRef} className="text-6xl font-bold">
+          {text.split("").map((letter, index) => (
+            <span key={index}>{letter}</span>
+          ))}
+        </h1>
       </div>
     </div>
   );
