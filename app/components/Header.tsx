dd4ae4bdecc1c39ef5dcb630f3c2cf186fda9618
André Montenegro <126609983+andre4383@users.noreply.github.com>
2026-02-12 18:04:17 -0300

    Arquivos

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="w-full py-2 px-2">
-      <h1 className="font-bold text-16pt text-gray-500 hover:text-black">
+      <h1 className="inline-block font-bold text-16pt text-gray-500 hover:text-black cursor-default transition-colors duration-300 ease-in-out">
         ok.
       </h1>
     </header>
