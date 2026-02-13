96ce87175f633cd8319221f290fbbb8969770694
André Montenegro <126609983+andre4383@users.noreply.github.com>
2026-02-12 23:00:34 -0300

    Arquivos

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,18 @@
+"use client";
 import React from "react";
-
+import Link from "next/link";
 export default function Header() {
   return (
     <header className="w-full py-2 px-2 flex">
       <h1 className="inline-block font-bold text-[16pt] text-gray-500 hover:text-black cursor-default transition-colors duration-300 ease-in-out selection:bg-black selection:text-white">
         ok.
       </h1>
-      <h2 className="inline-block font-bold text-[12pt] text-gray-500 hover:text-black cursor-default transition-colors duration-300 ease-in-out selection:bg-black selection:text-white ml-auto">
+      <Link
+        href="https://github.com/andre4383"
+        className="inline-block font-bold text-[12pt] text-gray-500 hover:text-black cursor-pointer transition-colors duration-300 ease-in-out selection:bg-black selection:text-white ml-auto"
+      >
         github
-      </h2>
+      </Link>
     </header>
   );
 }
